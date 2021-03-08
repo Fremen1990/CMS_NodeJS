f1c734a70a1f565b0480a6c04d87da00fc0625aa
Fremen1990 <fremen1990@gmail.com>
2021-03-09 00:04:07 +0100

    API

--- app.js
+++ app.js
@@ -1,42 +1,51 @@
-var createError = require('http-errors');
+var createError = require('http-errors')
 var cookieSession = require('cookie-session')
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 const config = require('./routes/config')
 var mongoose = require('mongoose')
 
-mongoose.connect(config.db, { useNewUrlParser: true });
+mongoose.connect(config.db, { useNewUrlParser: true })
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:!!!!!!!!!---------!!!!!!!!!!!!!'));
+const db = mongoose.connection
+db.on(
+  'error',
+  console.error.bind(
+    console,
+    'connection error:!!!!!!!!!---------!!!!!!!!!!!!!'
+  )
+)
 db.once('open', function () {
   console.log('DB CONNECTED')
-});
+})
 
-var indexRouter = require('./routes/index');
-var newsRouter = require('./routes/news');
-var quizRouter = require('./routes/quiz');
-var adminRouter = require('./routes/admin');
+var indexRouter = require('./routes/index')
+var newsRouter = require('./routes/news')
+var quizRouter = require('./routes/quiz')
+var adminRouter = require('./routes/admin')
+var apiRouter = require('./routes/api')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(cookieSession({
-  name: 'session',
-  keys: config.keySession,
-  maxAge: config.maxAgeSession
-}))
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: config.keySession,
+    maxAge: config.maxAgeSession
+  })
+)
 
 // app.use(function (req, res, next) {
 //   // console.log(req.path);
@@ -47,25 +56,26 @@ app.use(cookieSession({
 
 // })
 
-app.use('/', indexRouter);
-app.use('/news', newsRouter);
-app.use('/quiz', quizRouter);
-app.use('/admin', adminRouter);
+app.use('/', indexRouter)
+app.use('/news', newsRouter)
+app.use('/quiz', quizRouter)
+app.use('/admin', adminRouter)
+app.use('/api', apiRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
